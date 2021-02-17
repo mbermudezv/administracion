10736de9cf73f06ccbec080a455418e381caeb64
mbermudezv <mauriciobermudez@hotmail.com>
2021-02-17 10:53:53 -0600

    Liceo 8 febrero

--- sql/insertExcelGestor.php
+++ sql/insertExcelGestor.php
@@ -36,12 +36,12 @@ if (!isset($_FILES['upexcel']['tmp_name']) || !in_array($_FILES['upexcel']['type
       $highestRow = $worksheet->getHighestRow();
       
       /*for ($row = 7; $row <= $highestRow; ++$row) {*/
-      for ($row = 0; $row <= $highestRow; ++$row) {
+      for ($row = 1; $row <= $highestRow; ++$row) {
 
-            $estudiante_Cedula = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
-            $estudiante_PrimerApellido = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
-            $estudiante_SegundoApellido = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
-            $estudiante_Nombre = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
+            $estudiante_Cedula = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
+            $estudiante_PrimerApellido = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+            $estudiante_SegundoApellido = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
+            $estudiante_Nombre = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
             $estudiante_Seccion = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
 
             $db = new insertExcel();
